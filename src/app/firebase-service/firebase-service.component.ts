4a5fc62b27fd8752d3fa9d097f52ed3f796744f0
Lars Mensching <133211689+Imiphon@users.noreply.github.com>
2024-02-13 11:52:17 -0500

    Add realtime game update feature using Firestore onSnapshot

--- src/app/firebase-service/firebase-service.component.ts
+++ src/app/firebase-service/firebase-service.component.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Firestore, collection, collectionData, onSnapshot } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -7,14 +7,60 @@ import { Observable } from 'rxjs';
 }) //makes this component global
 export class FirestoreService {
 
-  //private firestore: Firestore
-  constructor(private firestore: Firestore) { }
+  game; 
+  singleGame;
+  //firstGame;
+
+  
+  constructor(private firestore: Firestore) { 
+    //onSnapshot needs a reference and a function
+    this.game = onSnapshot(this.gameReference(), (games) => {
+      // Iteration über jedes empfangene Spiel in Sammlung 'games' und Ausführung einer Aktion 
+      games.forEach(element => {
+        console.log('current game with onSnapshot: ', element);
+      })
+    });
+
+    this.singleGame = onSnapshot(this.gameReference(), (snapshot) => {
+      // Iteration über jedes empfangene Spiel in Sammlung 'games' und Ausführung einer Aktion
+      snapshot.forEach(doc => {
+        // Zugriff auf die Daten des Dokuments
+        const data = doc.data();
+        // Ausgabe des Werts des Felds 'game'
+        console.log('current game with onSnapshot: ', data);
+      })
+    });
+
+/**
+ *     //try to get single element
+    this.firstGame = onSnapshot(('games', '0qpdykQbqgFVuPijwoAb'), (element) => {
+
+    })
+ */
+
+  }
+
+  gameReference() {
+    return collection(this.firestore, 'games');
+  }
 
   getItems(): Observable<any[]> {
-    const gameCollection = collection(this.firestore, 'games');
+    const gameCollection = collection(this.firestore, 'game');
     const dataCollection = collectionData(gameCollection);  
-    console.log(dataCollection);
-     
+    console.log('with Observable', dataCollection);
     return  dataCollection;
   }
+
+      /**
+         // Dies ist eine ASYNC PIPE 
+         //Initialisierung des Observables mit Daten aus der Firestore-Sammlung 'notes'
+        this.items$ = collectionData(this.getNotesRef());
+        // Abonnieren des Observables, um auf Änderungen in der 'notes'-Sammlung zu reagieren
+        this.items = this.items$.subscribe((list) => {
+          // Iteration über jede empfangene Notiz und Ausführung einer Aktion (hier: Ausgabe in der Konsole)
+          list.forEach(element => {
+            console.log('Initialisierung des Observables mit collactionData()', element);
+          })
+        });
+     */
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,12 +12,3 @@ export const environment = {
     }
   };
 
-
-  /**
-   *       apiKey: "AIzaSyD4yTB6NEe_jKqg6l-7loT_KCgI-MaCvXs",
-      authDomain: "ring-of-fire-87981.firebaseapp.com",
-      projectId: "ring-of-fire-87981",
-      storageBucket: "ring-of-fire-87981.appspot.com",
-      messagingSenderId: "426381848920",
-      appId: "1:426381848920:web:ae73552bb7b7d69118752d"
-   */
